bf4796246a3ef09c0a840e12c56d13700301b5f7
truong202 <88317566+truong202@users.noreply.github.com>
2024-03-11 00:30:53 +0700

    add MovementCondition

---
+++ Assets/Scripts/Base/StateMachines/Conditions/MovementCondition.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public enum MovementState
+{
+    Run,
+    Stop
+}
+
+public class MovementConditionInfo
+{
+    public string state;
+}
+
+public class MovementCondition : Condition<MovementConditionInfo>
+{
+    protected MovementState state;
+
+    public override void SetInfo(object info)
+    {
+        base.SetInfo(info);
+        state = Enum.Parse<MovementState>(this.info.state);
+    }
+
+
+    protected override void CheckCondition()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+        Vector2 direction = new Vector2(x, y);
+
+        bool isStop = direction.Equals(Vector2.zero);
+
+        if (state == MovementState.Run)
+        {
+            SuitableCondition(!isStop);
+        }
+        else
+        {
+            SuitableCondition(isStop);
+        }
+
+    }
+}
+
