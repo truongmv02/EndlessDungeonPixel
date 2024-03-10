cf383dc06a8f6f2ed1a712ca267440ec11a7218c
truong202 <88317566+truong202@users.noreply.github.com>
2024-03-11 00:44:23 +0700

    create classe to load state machine data

---
+++ Assets/Scripts/Datas/DataManager.cs
@@ -0,0 +1,14 @@
+﻿public class DataManager : Singleton<DataManager>
+{
+    public StateMachineDatas StateMachineDatas { private set; get; }
+    public DataManager()
+    {
+        LoadData();
+    }
+    public virtual void LoadData()
+    {
+        //bulletDatas = new BulletDatas();
+        StateMachineDatas = new StateMachineDatas();
+
+    }
+}
---
+++ Assets/Scripts/Datas/StateMachine/StateMachineDatas.cs
@@ -0,0 +1,16 @@
+﻿
+
+using UnityEngine;
+
+public class StateMachineDatas : BaseMultiData
+{
+    public StateMachineDatas()
+    {
+        LoadData<StateMachineData>("Data/StateMachines");
+    }
+
+    public StateMachineInfo GetInfo(string[] paths)
+    {
+        return ((StateMachineData)Datas[paths[0]])?.GetStateMachineInfo(paths[1]);
+    }
+}
