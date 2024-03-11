af6a75c9c3d7c617ac579d5d04158dede4f5c10b
truong202 <141312017+truongmv02@users.noreply.github.com>
2024-03-11 09:37:57 +0700

    Create class TypeMoveController, MoveUp, Update PistolBullet prefab

---
+++ Assets/Scripts/Base/Movement/TypeMoveController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+public interface ITypeMove
+{
+    Vector2 Move();
+}
+
+[DisallowMultipleComponent]
+public class TypeMoveController : MoveController
+{
+    ITypeMove[] typeMoves;
+
+    protected override void Start()
+    {
+        base.Start();
+        typeMoves = GetComponents<ITypeMove>();
+    }
+
+    private void Update()
+    {
+        if (!CanMove) return;
+
+        Vector2 direction = Vector2.zero;
+
+        foreach (var typeMove in typeMoves)
+        {
+            direction += typeMove.Move();
+        }
+        _rigidbody.transform.up = direction;
+        Move(direction);
+    }
+}
