26485c83a5d3794a78d45ab1fab381615ccedd7e
truong202 <141312017+truongmv02@users.noreply.github.com>
2024-03-14 04:19:48 +0700

    Create Weapon Sword
    Animation, WeaponSlashState, Data

---
+++ Assets/Scripts/Weapons/States/WeaponSlashState.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class WeaponSlashState : State
+{
+
+    public override void Enter()
+    {
+        base.Enter();
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+
+        foreach (var condition in conditions)
+        {
+            condition.ResetCondition();
+
+        }
+    }
+
+}
